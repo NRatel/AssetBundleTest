da9d33d01939f4777b0bc5d95a5a858150cdf883
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-11-30 19:30:23 +0800

    Update BuildAB2.cs

--- Assets/Test2/Editor/BuildAB2.cs
+++ Assets/Test2/Editor/BuildAB2.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-//探究 SpriteAtlas 对 AssetBundle打包的影响
+//探究 图集（SpriteAtlas）对 AssetBundle打包的影响
 public class BuildAB2 : Editor
 {
     // UINotPack.prefab 引用的 3个Sprite 没有在任何图集中
     // UIPacked.prefab 引用的  3个Sprite 在 图集 Hero 中(Hero图集中有5个Sprite，Hero图集本身没打ab)
     // 结果：
     // UINotPack.ab 中 打入了 3个Sprite（含3个小Texture）
-    // UIPacked.ab 中 打入了 5个Sprite（含1个大Texture）, 相当于图集被完整打入ab
+    // UIPacked.ab 中 打入了 5个Sprite（含1个大Texture）, 相当于图集被打入ab
+    // 结论：
+    // 如果预设依赖了图集中的Sprite，且图集没打ab，则图集会被打入预设的ab中。
     [MenuItem("NRatel/BuildAB/Test2_1")]
     static public void Test2_1()
     {
