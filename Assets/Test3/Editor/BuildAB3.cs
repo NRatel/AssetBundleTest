10bc3491d78c84fcabe9033ec7b4c787d37d853d
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-12-02 18:18:24 +0800

    Test3

---
+++ Assets/Test3/Editor/BuildAB3.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+//探究 AssetBundle 循环依赖问题
+
+//结果： Manifest 中的依赖关系为空
+//ManifestFileVersion: 0
+//CRC: 1939429471
+//AssetBundleManifest:
+//  AssetBundleInfos:
+//    Info_0:
+//      Name: bundle1
+//      Dependencies: {}
+//    Info_1:
+//      Name: bundle2
+//      Dependencies: {}
+public class BuildAB3 : Editor
+{
+    [MenuItem("NRatel/BuildAB/Test3")]
+    static public void Build3()
+    {
+        BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+
+        string outputPath = ABBuildHelper.PrepareOutputFolder("Test3", buildTarget);
+
+        //清理所有的ABName
+        ABBuildHelper.ClearAllABNames();
+
+        //设置新的ABName
+        ABBuildHelper.SetABName("Assets/Test3/Prefabs/RawImageA.prefab", "bundle1", ".ab");
+        ABBuildHelper.SetABName("Assets/Test3/Textures/TextureY.png", "bundle1", ".ab");
+
+        ABBuildHelper.SetABName("Assets/Test3/Prefabs/RawImageB.prefab", "bundle2", ".ab");
+        ABBuildHelper.SetABName("Assets/Test3/Textures/TextureX.png", "bundle2", ".ab");
+
+        //开始构建
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, buildTarget);
+        
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Test3/Scripts/LoadAB1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class LoadAB3 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
